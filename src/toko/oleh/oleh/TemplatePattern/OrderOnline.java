497e8f0b22d8c5960e9ea36fea23a2c953809468
noviani-ulm <74365930+noviani-ulm@users.noreply.github.com>
2020-11-17 00:23:35 +0800

    Menambahkan Main

---
+++ src/toko/oleh/oleh/TemplatePattern/OrderOnline.java
@@ -0,0 +1,46 @@
+package toko.oleh.oleh.TemplatePattern;
+import java.util.Scanner;
+
+public class OrderOnline extends OrderTemplate{
+    @Override
+
+    public void doSelect() 
+
+    { 
+
+        System.out.println("Item added to online shopping cart"); 
+
+        System.out.println("Get gift wrap preference"); 
+
+        System.out.println("Get delivery address."); 
+
+    } 
+
+  
+
+    @Override
+
+    public void doPayment() 
+
+    { 
+
+        System.out.println 
+
+                   ("Online Payment through Netbanking, card or Paytm"); 
+
+    } 
+
+  
+
+    @Override
+
+    public void doDelivery() 
+
+    { 
+
+        System.out.println ("Ship the item through post to delivery address"); 
+
+    } 
+
+  
+} 
---
+++ src/toko/oleh/oleh/TemplatePattern/OrderTemplate.java
@@ -0,0 +1,67 @@
+
+package toko.oleh.oleh.TemplatePattern;
+
+
+public abstract class OrderTemplate 
+{ 
+
+    public boolean isGift; 
+
+  
+
+    public abstract void doSelect(); 
+
+  
+
+    public abstract void doPayment(); 
+
+  
+
+    public final void giftWrap() 
+
+    { 
+
+        try
+
+        { 
+
+            System.out.println("Gift wrap successful"); 
+
+        } 
+
+        catch (Exception e) 
+
+        { 
+
+            System.out.println("Gift wrap unsuccessful"); 
+
+        } 
+
+    } 
+
+  
+
+    public abstract void doDelivery(); 
+
+  
+
+    public final void processOrder(boolean isGift) 
+
+    { 
+
+        doSelect(); 
+
+        doPayment(); 
+
+        if (isGift) { 
+
+            giftWrap(); 
+
+        } 
+
+        doDelivery(); 
+
+    } 
+} 
+
+

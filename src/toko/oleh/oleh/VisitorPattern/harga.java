91aaa320af14df68323dde36c93ca575b130689b
taniasarah <niah.wara@gmail.com>
2020-11-17 03:34:34 +0800

    menambahkan interface oleh-oleh yang berbeda untuk digunakan dalam keranjang belanjaan dan class daftar harga oleh-oleh

---
+++ src/toko/oleh/oleh/VisitorPattern/harga.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toko.oleh.oleh.VisitorPattern;
+
+/**
+ *
+ * @author ASUS
+ */
+public class harga {
+    String amplang;
+    String apam;
+    String wajik;
+    String rimpi;
+    String ikan_rabuk;
+    String ikan_asin;
+    String dodol;
+    String wadai_rangai;
+    
+    void run(){
+        System.out.println("Daftar harga oleh-oleh");
+        System.out.println();
+        System.out.println("Amplang = " + amplang);
+        System.out.println("Apam = " + apam);
+        System.out.println("Wajik = " + wajik);
+        System.out.println("Rimpi = " + rimpi);
+        System.out.println("Ikan Rabuk = " + ikan_rabuk);
+        System.out.println("Ikan Asin = " + ikan_asin);
+        System.out.println("Dodol = " + dodol);
+        System.out.println("Wadai Rangai = " + wadai_rangai);
+        System.out.println();
+        System.out.println("============================================");
+        System.out.println("Oleh-oleh yang dibeli");
+        System.out.println();
+    }
+}
+
